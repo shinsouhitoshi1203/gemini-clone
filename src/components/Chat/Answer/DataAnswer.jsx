1354f9b05213dc16bafa4dd4195ab6a7629e8c5f
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2025-01-28 12:59:14 +0700

    gemini-clone [pj/ui] fixed: scrolling bug; divided: chatbox -> two independent comp

--- src/components/Chat/Answer/DataAnswer.jsx
+++ src/components/Chat/Answer/DataAnswer.jsx
@@ -1,89 +1,18 @@
-import remarkGfm from "remark-gfm";
-import useHome from "../../../hooks/useHome";
-import Button from "../../Button";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import ThumbDownIcon from "@mui/icons-material/ThumbDown";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import VolumeUpIcon from "@mui/icons-material/VolumeUp";
-import Markdown from "markdown-to-jsx";
-import {
-	useCallback,
-	useContext,
-	useEffect,
-	useMemo,
-	useRef,
-	useState
-} from "react";
-import { Scrl } from "../../../pages/Home/Home";
+import Options from "./Options";
+import RawAnswer from "./RawAnswer";
 
 function DataAnswer({ msg, req }) {
-	// actualText
-	const scroll = useContext(Scrl);
-	const [displayText, setDisplayText] = useState("");
-	const wordList = useMemo(() => {
-		return msg.split(" ");
-	}, []);
-
-	useEffect(() => {
-		if (req) {
-			async function fn() {
-				for (let i = 0; i < wordList.length; ++i) {
-					await new Promise((ok, nope) => {
-						setTimeout(() => {
-							scroll.current.scrollTo(
-								0,
-								scroll.current.scrollHeight
-							);
-							ok();
-						}, 60);
-					});
-					setDisplayText((x) => x + " " + wordList[i]);
-				}
-			}
-			fn();
-		}
-	}, []);
 	return (
 		<>
 			<div className="ChatBox__answer">
-				<Markdown
-					// remarkPlugins={[remarkGfm]}
-					style={{ font: "initial" }}
-					children={req ? displayText : msg}
-				/>
+				{req ? (
+					<RawAnswer needDelay={true} text={msg} />
+				) : (
+					<RawAnswer text={msg} />
+				)}
 			</div>
 			<div className="ChatBox__Options">
-				<Button
-					size="36 36 50%"
-					caption="Like this answer"
-					icon={<ThumbUpIcon />}
-					type="main"
-				></Button>
-				<Button
-					size="36 36 50%"
-					caption="Like this answer"
-					icon={<ThumbDownIcon />}
-					type="main"
-				></Button>
-				<Button
-					size="36 36 50%"
-					caption="Like this answer"
-					icon={<ShareIcon />}
-					type="main"
-				></Button>
-				<Button
-					size="36 36 50%"
-					caption="Like this answer"
-					icon={<VolumeUpIcon />}
-					type="main"
-				></Button>
-				<Button
-					size="36 36 50%"
-					caption="Like this answer"
-					icon={<MoreVertIcon />}
-					type="main"
-				></Button>
+				<Options />
 			</div>
 		</>
 	);
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,37 +7,32 @@ import HomeProvider from "../../contexts/HomeProvider";
 import "./../../assets/scss/pages/Home/Home.scss";
 import useHome from "../../hooks/useHome";
 import Chatbox from "./Chatbox";
-import { createContext, useRef } from "react";
 
 function Home() {
 	const { allowChat } = useHome();
-	const scroll = useRef(null);
 
 	return (
-		<Scrl.Provider value={scroll}>
-			<div className="pageHome router">
-				<div className="pageHome__display" ref={scroll}>
-					<div className="page__wrapper">
-						{allowChat ? <Chatbox /> : <Welcome />}
-					</div>
-				</div>
-				<div className="pageHome__input">
-					<div className="page__wrapper">
-						<Input />
-						<p className="pageHome__disclaimer font-4">
-							Gemini may display inaccurate info, including about
-							people, so double-check its responses.
-							<HyperLink
-								href="/privacy"
-								text="Your privacy & Gemini Apps"
-								cls="pageHome__privacy"
-							/>
-						</p>
-					</div>
+		<div className="pageHome router">
+			<div className="pageHome__display">
+				<div className="page__wrapper">
+					{allowChat ? <Chatbox /> : <Welcome />}
 				</div>
 			</div>
-		</Scrl.Provider>
+			<div className="pageHome__input">
+				<div className="page__wrapper">
+					<Input />
+					<p className="pageHome__disclaimer font-4">
+						Gemini may display inaccurate info, including about
+						people, so double-check its responses.
+						<HyperLink
+							href="/privacy"
+							text="Your privacy & Gemini Apps"
+							cls="pageHome__privacy"
+						/>
+					</p>
+				</div>
+			</div>
+		</div>
 	);
 }
 export default Home;
-export const Scrl = createContext();
