777003817a370a0deaa8b3f10cbc677d5b4855b1
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2025-01-29 21:50:40 +0700

    gemini-clone [app/routing] --temp: automatically navigate to /app when the url is '/'

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 
 import Main from "./pages/App";
 import Home from "./pages/App/Home";
+import RootPage from "./pages";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="*" element={<div> FUCK </div>} />
-			<Route path={"/app"} element={<Main />}>
-				<Route index element={<Home />}></Route>
+			<Route path="/" element={<RootPage />} />
+			<Route path="/app" element={<Main />}>
+				<Route index element={<Home />} />
 			</Route>
 		</Routes>
 	);
